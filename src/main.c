5a68e5ce20ddee0b106a295c4c77d19f01c4e7fd
Radik Zangirov <owtmpatrick@gmail.com>
2025-03-16 17:46:41 +0500

    define constants and variables

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+#define WIDTH 80
+#define HEIGHT 25
+#define PADDLE_SIZE 3
+#define BALL_SIZE 1
+#define WINNING_SCORE 21
+#define PADDLE_OFFSET 10
+
 int main() {
-  printf("let's get it started");
+  char field[HEIGHT][WIDTH + 1];
+  int paddle1_y = HEIGHT / 2 - 1;
+  int paddle2_y = HEIGHT / 2 - 1;
+  int ball_x = WIDTH / 2 - 1;
+  int ball_y = HEIGHT / 2;
+  int player1_score = 0;
+  int player2_score = 0;
+  int ball_direction_x = 1;
+  int ball_direction_y = 1;
+
   return 0;
 }
