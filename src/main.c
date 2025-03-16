25f405305fca9e56c90abc7a0e09534a55702b50
Radik Zangirov <owtmpatrick@gmail.com>
2025-03-16 21:03:55 +0500

    add main logic

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #define HEIGHT 25
 #define PADDLE_SIZE 3
 #define BALL_SIZE 1
-#define WINNING_SCORE 21
+#define WINNING_SCORE 1
 #define PADDLE_OFFSET 10
 
 void clear_field(char field[HEIGHT][WIDTH + 1]) {
@@ -38,7 +38,7 @@ void set_initial_state(int *ball_x, int *ball_y, int *paddle1_y, int *paddle2_y)
 }
 
 void update_ball(int *ball_x, int *ball_y, int *ball_direction_x, int *ball_direction_y, int *paddle1_y,
-                 int *paddle2_y, int player1_score, int player2_score) {
+                 int *paddle2_y, int *player1_score, int *player2_score) {
     *ball_x += *ball_direction_x;
     *ball_y += *ball_direction_y;
 
@@ -56,12 +56,12 @@ void update_ball(int *ball_x, int *ball_y, int *ball_direction_x, int *ball_dire
     }
 
     if (*ball_x == WIDTH - 1) {
-        player1_score += 1;
+        *player1_score += 1;
         set_initial_state(ball_x, ball_y, paddle1_y, paddle2_y);
     }
 
     if (*ball_x == 0) {
-        player2_score += 1;
+        *player2_score += 1;
         set_initial_state(ball_x, ball_y, paddle1_y, paddle2_y);
     }
 }
@@ -125,11 +125,23 @@ int main() {
     int ball_direction_x = 1;
     int ball_direction_y = 1;
 
-    clear_field(field);
+    while (player1_score < WINNING_SCORE || player2_score < WINNING_SCORE) {
+        draw(field, ball_x, ball_y, paddle1_y, paddle2_y, player1_score, player2_score);
 
-    move_paddle('k', &paddle1_y, &paddle2_y);
+        char input;
+        input = getchar();
 
-    draw(field, ball_x, ball_y, paddle1_y, paddle2_y, player1_score, player2_score);
+        move_paddle(input, &paddle1_y, &paddle2_y);
+
+        update_ball(&ball_x, &ball_y, &ball_direction_x, &ball_direction_y, &paddle1_y, &paddle2_y,
+                    &player1_score, &player2_score);
+
+        if (player1_score >= WINNING_SCORE) {
+            printf("Player 1 wins!\n");
+        } else if (player2_score >= WINNING_SCORE) {
+            printf("Player 2 wins!\n");
+        }
+    }
 
     return 0;
 }
